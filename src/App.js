1252e6b35d1f16b3915e6b2eec879c33cc1f500b
Jati-jostar <jati8676@gmail.com>
2026-07-27 23:22:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Button } from 'react-bootstrap'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="py-5 text-center">
+      <h1>Project React Pertamaku</h1>
+      <p>Aku sedang belajar React dan Bootstrap.</p>
+      <Button variant="primary">Mulai Belajar</Button>
+    </Container>
+  )
 }
 
-export default App;
+export default App

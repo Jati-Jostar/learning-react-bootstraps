7da197402ba3e7b83c5ff2e292f81e4ce9bf0fa3
fida.jati <fida.jati@solusi247.com>
2026-07-28 16:40:14 +0700

    up new file hero.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Header from "./components/header"
+import Header from "./components/header";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <div className="halaman">
       <Header />
-      
+      <Hero />
+
       <main id="home">
         <h2>Belajar React dari Nol</h2>
         <p>ini isi utama website</p>
---
+++ src/components/hero.js
@@ -0,0 +1,17 @@
+function Hero () {
+    return (
+        <header>
+            <h1>
+                Belajar React dari Nol
+            </h1>
+            <p>
+                Ini Adalah Project React Pertama Saya.
+            </p>
+            <button>
+                Mulai Belajar
+            </button>
+        </header>
+    )
+}
+
+export default Hero;
